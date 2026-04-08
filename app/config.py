6fe61ef0a2a27f90f695fedb5a161cf5eb17a094
Michael Rosendal <65503882+mbarosendal@users.noreply.github.com>
2026-04-08 15:10:57 +0200

    Rename in config

--- app/config.py
+++ app/config.py
@@ -4,17 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env file
 
 class Config:
-<<<<<<< HEAD:config.py
     # Switch to enforce project constraints
     # USE_TEST_MODE = True
     IS_TEST_PROJECT_ONLY = True
     DALUX_SCOPED_PROJECT_ID = os.getenv("DALUX_PROJECT_ID")
-=======
-    # Switches to enforce project constraints 
-    # TEST_MODE_ON = True
-    USE_TEST_PROJECT_ONLY = True
-    DALUX_PROJECT_ID = os.getenv("DALUX_PROJECT_ID")
->>>>>>> ca56b6ee0ce032f78858d3779f0638cd48aa3a2e:app/config.py
 
     DALUX_BASE_URL = os.getenv("DALUX_BASE_URL")
     DALUX_API_KEY = os.getenv("DALUX_API_KEY")
