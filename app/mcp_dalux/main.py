2c0aaffed7d43edd1c3fbe5fa26ff8d2845696ea
Michael Rosendal <65503882+mbarosendal@users.noreply.github.com>
2026-04-10 08:49:09 +0200

    Removed test code from main

--- app/mcp_dalux/main.py
+++ app/mcp_dalux/main.py
@@ -12,21 +12,6 @@ def main() -> None:
     try:
         Config.validate()
 
-        # adapter = DaluxAdapter()
-        # tasks = adapter.get_tasks(Config.DALUX_SCOPED_PROJECT_ID) # test
-        # tasks = adapter.get_task("S432130555081916416", Config.DALUX_SCOPED_PROJECT_ID) # test
-        # tasks = adapter.get_task_attachments(Config.DALUX_SCOPED_PROJECT_ID) # test
-        # tasks = adapter.get_task_changes(Config.DALUX_SCOPED_PROJECT_ID) # test
-
-        # with open("full.json", "w", encoding="utf-8") as f:
-        #     json.dump(tasks, f, indent=2, ensure_ascii=False)
-        # print("Saved full response to full.json")
-
-        # if not tasks:
-        #         print("No task(s) found.")
-
-        # print(json.dumps(tasks, indent=2, ensure_ascii=False)) #test
-        # return
     except ValueError as e:
         logger.error(f"Configuration error: {e}")
         print(f"Configuration error: {e}", file=sys.stderr)
